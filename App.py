56ebeceb38a2a3365b8f064486cb8de0f64f5f1d
l0hitk <127608339+l0hitk@users.noreply.github.com>
2024-03-31 23:10:34 +0530

    First Commit

---
+++ App.py
@@ -0,0 +1,251 @@
+import streamlit as st
+import nltk
+import spacy
+nltk.download('stopwords')
+nlp = spacy.load("en_core_web_sm", prefer_gpu=False)
+
+import pandas as pd
+import base64
+import time
+from pyresparser import ResumeParser
+from pdfminer3.layout import LAParams
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+import io
+from PIL import Image
+import pymysql
+
+
+def get_table_download_link(df, filename, text):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
+    return href
+
+
+def pdf_reader(file):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(file, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+            print(page)
+        text = fake_file_handle.getvalue()
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+    return text
+
+
+def show_pdf(file_path):
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+
+connection = pymysql.connect(host='localhost', user='root', password='')
+cursor = connection.cursor()
+
+
+def insert_data(name, email, res_score, no_of_pages, cand_level, skills):
+    if name is None or name.strip() == '':
+        name = 'N/A'
+    if email is None or email.strip() == '':
+        email = 'N/A'
+    if skills is None or skills.strip() == '':
+        skills = 'N/A'
+
+    DB_table_name = 'user_data'
+    insert_sql = f"INSERT INTO {DB_table_name} (Name, Email_ID, resume_score, Page_no, User_level, Actual_skills) VALUES (%s, %s, %s, %s, %s, %s)"
+    rec_values = (name, email, res_score, no_of_pages, cand_level, skills)
+    cursor.execute(insert_sql, rec_values)
+    connection.commit()
+
+st.set_page_config(
+    page_title="Smart Resume Analyzer",
+    page_icon='./Logo/SRA_Logo.ico',
+)
+
+
+def run():
+    st.title("Smart Resume Analyser")
+    st.sidebar.markdown("# Choose User")
+    activities = ["Normal User", "Admin"]
+    choice = st.sidebar.selectbox("Choose among the given options:", activities)
+    img = Image.open('./Logo/SRA_Logo.jpg')
+    img = img.resize((250, 250))
+    st.image(img)
+
+    # Create the DB
+    db_sql = """CREATE DATABASE IF NOT EXISTS SRA;"""
+    cursor.execute(db_sql)
+    connection.select_db("sra")
+
+    # Create table
+    DB_table_name = 'user_data'
+    table_sql = "CREATE TABLE IF NOT EXISTS " + DB_table_name + """
+                    (ID INT NOT NULL AUTO_INCREMENT,
+                     Name varchar(100) NOT NULL,
+                     Email_ID VARCHAR(50) NOT NULL,
+                     resume_score VARCHAR(8) NOT NULL,
+                     Page_no VARCHAR(5) NOT NULL,
+                     User_level VARCHAR(30) NOT NULL,
+                     Actual_skills VARCHAR(300) NOT NULL,
+                     PRIMARY KEY (ID));
+                    """
+    cursor.execute(table_sql)
+    if choice == 'Normal User':
+        pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+        if pdf_file is not None:
+            save_image_path = './Uploaded_Resumes/' + pdf_file.name
+            with open(save_image_path, "wb") as f:
+                f.write(pdf_file.getbuffer())
+            show_pdf(save_image_path)
+            resume_data = ResumeParser(save_image_path).get_extracted_data()
+            if 'name' in resume_data and resume_data['name'] is not None:
+                st.success("Hello " + resume_data['name'])
+            else:
+                st.success("Hello")
+            if resume_data:
+                resume_text = pdf_reader(save_image_path)
+                st.header("**Resume Analysis**")
+                if 'name' in resume_data and resume_data['name'] is not None:
+                    st.success("Hello " + resume_data['name'])
+                else:
+                    st.success("Hello")
+                st.subheader("**Your Basic info**")
+                try:
+                    st.text('Name: ' + resume_data['name'])
+                    st.text('Email: ' + resume_data['email'])
+                    st.text('Contact: ' + resume_data['mobile_number'])
+                    st.text('Resume pages: ' + str(resume_data['no_of_pages']))
+                except:
+                    pass
+                cand_level = ''
+                if resume_data['no_of_pages'] == 1:
+                    cand_level = "Fresher"
+                    st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>You are looking Fresher.</h4>''',
+                                unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] == 2:
+                    cand_level = "Intermediate"
+                    st.markdown('''<h4 style='text-align: left; color: #1ed760;'>You are at intermediate level!</h4>''',
+                                unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] >= 3:
+                    cand_level = "Experienced"
+                    st.markdown('''<h4 style='text-align: left; color: #fba171;'>You are at experience level!''',
+                                unsafe_allow_html=True)
+
+                ### Resume writing recommendation
+                st.subheader("**Resume Tips & Ideas💡**")
+                resume_score = 0
+                if 'Objective' in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added Objective</h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add your career objective, it will give your career intension to the Recruiters.</h4>''',
+                        unsafe_allow_html=True)
+
+                if 'CERTIFICATION' or 'Certification' in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have completed few certification✍/h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add few certification✍. It will give the assurance that you have some real time knowlegde</h4>''',
+                        unsafe_allow_html=True)
+
+                if 'Hobbies' or 'Interests' in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added your Hobbies⚽</h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Hobbies⚽. It will show your persnality to the Recruiters and give the assurance that you are fit for this role or not.</h4>''',
+                        unsafe_allow_html=True)
+
+                if 'Achievements' or 'ACHIEVEMENTS' in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added your Achievements🏅 </h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Achievements🏅. It will show that you are capable for the required position.</h4>''',
+                        unsafe_allow_html=True)
+
+                if 'Projects' in resume_text:
+                    resume_score = resume_score + 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added your Projects👨‍💻 </h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Projects👨‍💻. It will show that you have done work related the required position or not.</h4>''',
+                        unsafe_allow_html=True)
+
+                st.subheader("**Resume Score📝**")
+                st.markdown(
+                    """
+                    <style>
+                        .stProgress > div > div > div > div {
+                            background-color: #d73b5c;
+                        }
+                    </style>""",
+                    unsafe_allow_html=True,
+                )
+                my_bar = st.progress(0)
+                score = 0
+                for percent_complete in range(resume_score):
+                    score += 1
+                    time.sleep(0.1)
+                    my_bar.progress(percent_complete + 1)
+                st.success('** Your Resume Writing Score: ' + str(score) + '**')
+                st.warning(
+                    "** Note: This score is calculated based on the content that you have added in your Resume. **")
+                st.balloons()
+
+                if resume_data['name'] and resume_data['email'] and resume_data['skills']:
+                    insert_data(resume_data['name'], resume_data['email'], str(resume_score),
+                                str(resume_data['no_of_pages']), cand_level, str(resume_data['skills']))
+                else:
+                    print("Error: One or more required fields are missing in the resume data.")
+
+
+                connection.commit()
+            else:
+                st.error('Something went wrong..')
+    else:
+        ## Admin Side
+        st.success('Welcome to Admin Side')
+        ad_user = st.text_input("Username")
+        ad_password = st.text_input("Password", type='password')
+        if st.button('Login'):
+            if ad_user == 'qwert123' and ad_password == '147258369':
+                st.success("Welcome Admin")
+                # Display Data
+                cursor.execute('''SELECT*FROM user_data''')
+                data = cursor.fetchall()
+                st.header("**User's👨‍💻 Data**")
+                df = pd.DataFrame(data, columns=['ID', 'Name', 'Email', 'Resume Score', 'Total Page', 'User Level', 'Actual Skills'])
+                st.dataframe(df)
+                st.markdown(get_table_download_link(df, 'User_Data.csv', 'Download Report'), unsafe_allow_html=True)
+                query = 'select * from user_data;'
+                plot_data = pd.read_sql(query, connection)
+            else:
+                st.error("Wrong ID & Password Provided")
+
+
+run()
---
+++ README.md
@@ -0,0 +1,29 @@
+# Smart Resume Analyser App
+
+## Overview
+This Python project aims to analyze resumes in PDF format using the PDFMiner library for extracting text from PDF files and PyResparser for parsing and extracting relevant information such as skills, experience, and education from resumes.
+
+## Features
+- User's & Admin Section
+- Resume Score
+- Resume writing Tips suggestions
+
+## Usage
+- Clone my repository.
+- Open CMD in working directory.
+- Run following command.
+  ```
+  pip install -r requirements.txt
+  ```
+- `App.py` is the main Python file of Streamlit Web-Application. 
+- Download XAMPP or any other control panel, and turn on the Apache & SQL service.
+- To run app, write following command in CMD. or use any IDE.
+  ```
+  streamlit run App.py
+  ```
+- `Uploaded_Resumes` folder is contaning the user's uploaded resumes.
+- Admin side credentials is `machine_learning_hub` and password is `mlhub123`. 
+
+## Acknowledgements
+- The PyResparser library by Omkar Pathak for providing resume parsing capabilities.
+- The PDFMiner library contributors for enabling PDF text extraction programmatically.
